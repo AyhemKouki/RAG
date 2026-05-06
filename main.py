303890d5e5658ea0a25c3984a27cb03e8a7d90a6
Ayhem Kouki <ayhemkouki542@gmail.com>
2026-05-06 07:45:23 +0100

    add LLM

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import PromptTemplate
+from langchain_groq import ChatGroq
+import os
+from dotenv import load_dotenv
+
+GROQ_MODEL = "llama-3.3-70b-versatile"
 
 # step 1 : load the PDF file and read it as a list of documents
 # create instance of PyPDFLoader class
-loader = PyPDFLoader("data.pdf")
+loader = PyPDFLoader("data/file.pdf")
 # Open and read PDF file : returns a list of documents (metadata , source ...)
 documents = loader.load()
 
@@ -34,8 +40,36 @@ print(f"Number of documents in the vector store: {len(db.docstore._dict)}")
 
 # step 5 : Retrieve relevant chunks from the vector store based on a query
 query = "La technologie Ethernet"
-docs = db.similarity_search(query)
+docs = db.similarity_search(query, k=3)
 
-for doc in docs:
-    print(doc.page_content)
-    print("\n---\n")
+# Load variables from .env file
+load_dotenv()
+groq_api_key = os.getenv("GROQ_API_KEY")
+
+llm = ChatGroq(model=GROQ_MODEL, api_key=groq_api_key)
+
+template = """
+    Tu es un assistant pedagogique specialise en IA generative.
+    Tu dois repondre uniquement a partir du contexte fourni.
+
+    Consignes importantes :
+    - Reponds en francais.
+    - Si l’information n’est pas presente dans le contexte, dis clairement :
+        "Je ne trouve pas cette information dans les documents fournis."
+    - Donne une reponse claire, structuree et concise.
+    - Termine par une ligne "Sources :" avec les fichiers utilises.
+
+    Contexte :
+    {context}
+
+    Question :
+    {question}
+ """
+
+context = "\n".join([doc.page_content for doc in docs])
+
+prompt = PromptTemplate.from_template(template)
+final_prompt = prompt.format(context=context, question=query)
+
+response = llm.invoke(final_prompt)
+print(response.content)
